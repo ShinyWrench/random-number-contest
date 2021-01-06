23769d03e4c30bdec7292e8ccae25d4ac3aedff8
Jason Peppers <jasonpeppers72@gmail.com>
2021-01-05 22:24:44 -0500

    initial commit; get simple scoreboard example working

---
+++ player.js
@@ -0,0 +1,56 @@
+const redis = require('redis');
+client = redis.createClient();
+
+const { promisify } = require('util');
+const hsetAsync = promisify(client.hset).bind(client);
+const incrbyAsync = promisify(client.incrby).bind(client);
+const hincrbyAsync = promisify(client.hincrby).bind(client);
+const zadd = promisify(client.zadd).bind(client);
+
+const redisKeys = {
+    playerID: 'playerID',
+    numSamples: 'numSamples',
+    sum: 'sum',
+    average: 'average',
+    scoreboard: 'scoreboard',
+};
+
+async function sleep_ms(duration_ms) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, duration_ms);
+    });
+}
+
+function chooseNumber() {
+    return 1 + Math.floor(Math.random() * 10);
+}
+
+async function play() {
+    // Increment the playerID value to retrieve our ID
+    let id = await incrbyAsync(redisKeys.playerID, 1);
+
+    // Initialize our average
+    await hsetAsync(id, redisKeys.average, 0);
+
+    let numSamples, sum;
+    while (true) {
+        // Increment sample count
+        numSamples = await hincrbyAsync(id, redisKeys.numSamples, 1);
+
+        // Choose a random number and add it to sum
+        sum = await hincrbyAsync(id, redisKeys.sum, chooseNumber());
+
+        // Store our new average
+        await hsetAsync(id, redisKeys.average, sum / numSamples);
+
+        // Update the scoreboard with our average
+        await zadd(redisKeys.scoreboard, sum / numSamples, id);
+
+        // Sleep 1 second
+        await sleep_ms(1000);
+    }
+}
+
+play().then(() => {});
